97d861b8c2b3b9bc8f3fb0d7be4834221bac6ed1
JithuPrabhakar <join.jitz@gmail.com>
2025-07-23 13:15:32 +0530

    minor changes in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import Register from "./components/Register";
 import Login from "./components/Login";
@@ -6,8 +5,6 @@ import About from "./components/About";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
